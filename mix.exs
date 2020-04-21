646e2232fe18e91414a745e462a2940a9038016d
Dan Schultzer <dan@dreamconception.com>
2020-04-21 10:42:59 -0700

    Install Pow

--- mix.exs
+++ mix.exs
@@ -44,7 +44,9 @@ defmodule MyApp.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+
+      {:pow, "~> 1.0.20"}
     ]
   end
 
