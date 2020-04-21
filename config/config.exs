4685efd49edcdfd0f1c63439b9626b37214b542f
Dan Schultzer <dan@dreamconception.com>
2020-04-21 10:33:36 -0700

    mix phx.new my_app

---
+++ config/config.exs
@@ -0,0 +1,31 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :my_app,
+  ecto_repos: [MyApp.Repo]
+
+# Configures the endpoint
+config :my_app, MyAppWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "ETRmQ/6I0At8Q3QIe6j46gnEOzby96RXvZ59L89qyPPo/w4S7H5wT8z2ja8GDEWo",
+  render_errors: [view: MyAppWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: MyApp.PubSub, adapter: Phoenix.PubSub.PG2],
+  live_view: [signing_salt: "bfqzsZIz"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
