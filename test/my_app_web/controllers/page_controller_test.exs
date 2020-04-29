297d8558b0c31105b7c357bf64c109ee07eb36a4
Dan Schultzer <dan@dreamconception.com>
2020-04-28 18:58:22 -0700

    Add controller test

--- test/my_app_web/controllers/page_controller_test.exs
+++ test/my_app_web/controllers/page_controller_test.exs
@@ -1,8 +1,30 @@
 defmodule MyAppWeb.PageControllerTest do
   use MyAppWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  @tenant "tenant_a"
+
+  describe "GET /" do
+    test "unauthenticated", %{conn: conn} do
+      conn = get(conn, "/", subdomain: @tenant)
+      assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+      assert conn.assigns[:current_tenant] == @tenant
+    end
+
+    test "authenticated", %{conn: conn} do
+      conn = create_auth_user(conn)
+
+      conn = get(conn, "/", subdomain: @tenant)
+      assert Pow.Plug.current_user(conn)
+      assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+      assert conn.assigns[:current_tenant] == @tenant
+    end
+  end
+
+  defp create_auth_user(conn) do
+    conn = post(conn, Routes.pow_registration_path(conn, :create, subdomain: @tenant), %{"user" => %{"email" => "test@example.com", "password" => "password", "password_confirmation" => "password"}})
+
+    :get
+    |> Plug.Test.conn("/")
+    |> Plug.Test.recycle_cookies(conn)
   end
 end
